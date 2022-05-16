393f22ee5d66a840d52278bb720d302158c38a99
Wellington Augusto Ribeiro <59797383+wellingtonaribeiro@users.noreply.github.com>
2022-05-16 13:38:04 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,16 +3,16 @@
 # npm -g i typescript ts-node
 
 
-#dotnet tool install --global dotnet-reportgenerator-globaltool
-#dotnet tool install --global dotnet-trace
-#dotnet tool install --global dotnet-sonarscanner
-#dotnet tool install --global dotnet-aspnet-codegenerator
-#dotnet tool install --global dotnet-counters
-#dotnet tool install --global dotnet-ef
-#dotnet tool install --global dotnet-monitor
+dotnet tool install --global dotnet-reportgenerator-globaltool
+dotnet tool install --global dotnet-trace
+dotnet tool install --global dotnet-sonarscanner
+dotnet tool install --global dotnet-aspnet-codegenerator
+dotnet tool install --global dotnet-counters
+dotnet tool install --global dotnet-ef
+dotnet tool install --global dotnet-monitor
 
-#<# chocolatey setup #> 
-#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
+<# chocolatey setup #> 
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 ​
 <# allowing globally confirmation #> 
 choco feature enable -n=allowGlobalConfirmation;
@@ -25,7 +25,7 @@ choco feature enable -n=allowGlobalConfirmation;
 ,"dotnetcore-sdk"
 ,"microsoft-edge"
 ,"visualstudio2022enterprise"
-#,"azure-functions-core-tools-4 --params "'/x64'""
+,"azure-functions-core-tools-4 --params "'/x64'""
 ,"microsoft-windows-terminal"
 ,"azure-cli"
 ,"gh"
